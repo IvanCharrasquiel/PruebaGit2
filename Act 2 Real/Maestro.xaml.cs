48e2685e2638a17e2c45fb198bc450d48f84e878
Lizethmercado <lizeth.mercadob@cecar.edu.co>
2024-10-13 21:56:01 -0500

    agregué un nuevo boton y una pagina muaak

--- Act 2 Real/Maestro.xaml.cs
+++ Act 2 Real/Maestro.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Act_2_Real.Pages;
 using Microsoft.Maui.Controls;
 
 namespace Act_2_Real
@@ -10,6 +11,25 @@ namespace Act_2_Real
         public Maestro()
         {
             InitializeComponent();
+        }
+
+        public static void OcultarDetalles()
+        {
+            App.FlyoutPage.IsPresented = false;
+        }
+
+
+        public static void NavigationToPage(ContentPage nuevaPagina)
+        {
+            App.FlyoutPage.Detail.Navigation.PushAsync(nuevaPagina);
+        }
+
+        private void GoToPaisesPage(object sender, EventArgs e)
+        {
+
+            NavigationToPage(new Paises());
+
+            OcultarDetalles();
         }
 
     }
