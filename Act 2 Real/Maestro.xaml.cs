300a7d651f0b17c2751801f928ba493d23a2a133
Yulianismelendez <yulianis.melendez@cecar.edu.co>
2024-10-13 22:01:31 -0500

    se agrego un nuevo boton

--- Act 2 Real/Maestro.xaml.cs
+++ Act 2 Real/Maestro.xaml.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Act_2_Real.Pages;
 using Microsoft.Maui.Controls;
 
 namespace Act_2_Real
@@ -11,6 +12,26 @@ namespace Act_2_Real
         {
             InitializeComponent();
         }
+
+        public static void NavigationToPage(ContentPage nuevaPagina)
+        {
+            App.FlyoutPage.Detail.Navigation.PushAsync(nuevaPagina);
+        }
+
+        public static void OcultarDetalles()
+        {
+            App.FlyoutPage.IsPresented = false;
+        }
+
+        private void GoToCafePage(object sender, EventArgs e)
+        {
+
+            NavigationToPage(new Cafe());
+
+            OcultarDetalles();
+        }
+
+
 
     }
 }
