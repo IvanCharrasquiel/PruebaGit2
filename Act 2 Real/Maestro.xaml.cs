1355d7a165dc50bb1851b550d39be1e4f24a72a8
IvanCharrasquiel <ivan.charrasquiel@cecar.edu.co>
2024-10-13 21:33:01 -0500

    Se sube el codigo base

---
+++ Act 2 Real/Maestro.xaml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Maui.Controls;
+
+namespace Act_2_Real
+{
+    public partial class Maestro : ContentPage
+    {
+        public Maestro()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
